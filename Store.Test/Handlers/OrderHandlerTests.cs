223806b58f12b987424ceecbc477b7eab7d088e5
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2020-11-02 16:53:18 -0300

    v13

--- Store.Test/Handlers/OrderHandlerTests.cs
+++ Store.Test/Handlers/OrderHandlerTests.cs
@@ -29,28 +29,91 @@ namespace store.test.Handlers
         [TestCategory("Handlers")]
         public void Dado_um_cliente_inexistente_o_pedido_nao_deve_ser_gerado()
         {
-            Assert.Fail();
+            var command = new CreateOrderCommand();
+            command.Custumer = "1234567891";
+            command.ZipCode = "1341108";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            
+            var handle = new OrderHandler(
+                _custumerRespository,
+                _deliveryRepository,
+                _discountRepository,
+                _orderRepository,
+                _productRepository
+            );
+
+            handle.Handle(command);
+
+            Assert.IsTrue(command.Invalid);
         }
 
         [TestMethod]
         [TestCategory("handlers")]
-        public void Dado_um_cep_invalido_o_pedido_deve_ser_gerado_normalmente()
+        public void Dado_um_cep_invalido_o_pedido_nao_deve_ser_gerado_normalmente()
         {
-            Assert.Fail();
+            var command = new CreateOrderCommand();
+            command.Custumer = "12345678911";
+            command.ZipCode = "134110889";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            
+            var handle = new OrderHandler(
+                _custumerRespository,
+                _deliveryRepository,
+                _discountRepository,
+                _orderRepository,
+                _productRepository
+            );
+
+            handle.Handle(command);
+
+            Assert.IsTrue(command.Invalid);
         }
 
         [TestMethod]
         [TestCategory("handlers")]
         public void Dado_um_promocode_inexistente_o_pedido_deve_ser_gerado_normalmente()
         {
-            Assert.Fail();
+            var command = new CreateOrderCommand();
+            command.Custumer = "12345678911";
+            command.ZipCode = "134110889";
+            command.PromoCode = "123456789";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            
+            var handle = new OrderHandler(
+                _custumerRespository,
+                _deliveryRepository,
+                _discountRepository,
+                _orderRepository,
+                _productRepository
+            );
+
+            handle.Handle(command);
+
+            Assert.IsTrue(handle.Valid);
         }
 
         [TestMethod]
         [TestCategory("handlers")]
         public void Dado_um_pedido_sem_itens_o_mesmo_nao_deve_ser_gerado()
         {
-            Assert.Fail();
+            var command = new CreateOrderCommand();
+            command.Custumer = "12345678911";
+            command.ZipCode = "12345678";
+            command.PromoCode = "12345678";
+            
+            var handle = new OrderHandler(
+                _custumerRespository,
+                _deliveryRepository,
+                _discountRepository,
+                _orderRepository,
+                _productRepository
+            );
+
+            handle.Handle(command);
+
+            Assert.IsTrue(handle.Valid);
         }
 
         [TestMethod]
@@ -58,8 +121,8 @@ namespace store.test.Handlers
         public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
         {
             var command = new CreateOrderCommand();
-            command.Custumer = " ";
-            command.ZipCode = "13411080";
+            command.Custumer = "";
+            command.ZipCode = "134110805";
             command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
             command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
             
